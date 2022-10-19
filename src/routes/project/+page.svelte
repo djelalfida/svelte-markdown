20ec962175c1f72b1e53f8a3eaa8e9988a591d30
djelal.fida <djelal.fida@hotmail.com>
2022-10-19 13:15:08 +0200

    New route for projects

---
+++ src/routes/project/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Card from '$lib/components/Card.svelte';
+</script>
+
+<h2>My personal projects</h2>
+
+<Card
+	image={'https://appstronauts.co/wp-content/uploads/2021/05/10-companies-using-svelte.jpg'}
+	title={'Random Placeholder for project'}
+	tags={'svelte javascript'}
+	description={'Some random description for the project'}
+/>
